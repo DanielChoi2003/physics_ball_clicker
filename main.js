ff9ee5e9e389db3c6341d941ad36cf9ae4864bb0
Daniel Choi <danilchoi03@gmail.com>
2026-01-02 11:14:44 -0600

    Add a button that resets the ball
    Add an event listener for the button that resets the ball's position, angular velocity, velocity,
    and velocity
    
    Only resetting the position will lead to the ball phasing through the wall because the ball's
    internal state will become inconsistent. Everything must be resetted.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const { Engine, Render, Runner, Bodies, Composite, Body, Vector } = Matter;
 
 const engine = Engine.create();
@@ -20,7 +21,9 @@ Runner.run(Runner.create(), engine);
 // x position, y position, radius, physical behavior
 // more on physical behavior: restitution determines how "bouncy" the ball is
 // the higher the value, the less energy loss the ball experiences after a collision
-const ball = Bodies.circle(400, 400, 30, { restitution: 0.9 });
+const originalX = width / 2;
+const originalY = height / 2;
+const ball = Bodies.circle(originalX, originalY, 30, { restitution: 0.9 });
 const ground = Bodies.rectangle(width / 2, height * 3 / 4, width, 40, { isStatic: true });
 const left_wall = Bodies.rectangle(0, height / 2,  40, height / 2, {isStatic: true});
 const right_wall = Bodies.rectangle(width, height / 2,  40, height / 2, {isStatic: true});
@@ -30,25 +33,34 @@ Composite.add(engine.world, [ball, ground, left_wall, right_wall, ceiling]);
 
 document.addEventListener("click", function(event) {
 
-    const xPos = event.clientX;
-    const yPos = event.clientY;
-    
-    const dist_to_clicked_pixel = Math.sqrt((xPos - ball.position.x) ** 2 + 
-                                            (yPos - ball.position.y) ** 2);
+  const xPos = event.clientX;
+  const yPos = event.clientY;
+  
+  const dist_to_clicked_pixel = Math.sqrt((xPos - ball.position.x) ** 2 + 
+                                          (yPos - ball.position.y) ** 2);
 
-    if(dist_to_clicked_pixel > ball.circleRadius){
-        return;
-    }
+  if(dist_to_clicked_pixel > ball.circleRadius){
+      return;
+  }
 
-
-    // dir.x determines the horizontal movement: positive -> right, negative -> left
-    const dir = Vector.normalise({
-        x: ball.position.x - xPos,
-        y: ball.position.y - yPos
-    });
+  // dir.x determines the horizontal movement: positive -> right, negative -> left
+  const dir = Vector.normalise({
+      x: ball.position.x - xPos,
+      y: ball.position.y - yPos
+  });
 
   Body.applyForce(ball, ball.position, {
     x: dir.x * (dist_to_clicked_pixel / ball.circleRadius) * 0.3,
     y: dir.y * (dist_to_clicked_pixel / ball.circleRadius) * 0.3
   });
+
+});
+
+const reset_button = document.getElementById("reset");
+
+reset_button.addEventListener("click", () => {
+  Body.setPosition(ball, {x: originalX, y: originalY});
+  Body.setAngle(ball, 0);
+  Body.setVelocity(ball, {x: 0, y: 0});
+  Body.setAngularVelocity(ball, 0);
 });
