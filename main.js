48272e19f532ae41708be5f92772e1b25f42f9c1
Daniel Choi <danilchoi03@gmail.com>
2026-01-01 23:54:22 -0600

    edited the folder and its file name from ball to button. Applied id to divs to render a button on top of the canvas created by matter-js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { Engine, Render, Runner, Bodies, Composite, Body, Vector } = Matter;
 const engine = Engine.create();
 
 const render = Render.create({
-  element: document.body,
+  element: document.getElementById("scene"),
   engine,
   options: {
     width: window.innerWidth,
@@ -20,9 +20,10 @@ Runner.run(Runner.create(), engine);
 
 const ball = Bodies.circle(400, 200, 30, { restitution: 0.9 });
 const ground = Bodies.rectangle(width / 2, height * 3 / 4, width, 40, { isStatic: true });
-const left_wall = Bodies.rectangle();
+const left_wall = Bodies.rectangle(0, height / 2,  40, height / 2, {isStatic: true});
+const right_wall = Bodies.rectangle(width, height / 2,  40, height / 2, {isStatic: true});
 
-Composite.add(engine.world, [ball, ground]);
+Composite.add(engine.world, [ball, ground, left_wall, right_wall]);
 
 document.addEventListener("click", () => {
   const dir = Vector.normalise({
