4d92fe41318182f170e54425e94ede0730c86e34
Daniel Choi <danilchoi03@gmail.com>
2026-01-02 00:23:55 -0600

    Enclosed the ball in a rectangular case. Instead of moving the ball in a random direction for any click on the screen, the click has to be on the ball and depending on location of the click on the ball, it pushes the ball with corresponding force and direction.

--- main.js
+++ main.js
@@ -17,22 +17,38 @@ const height = render.options.height;
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
-
-const ball = Bodies.circle(400, 200, 30, { restitution: 0.9 });
+// x position, y position, radius, physical behavior
+// more on physical behavior: restitution determines how "bouncy" the ball is
+// the higher the value, the less energy loss the ball experiences after a collision
+const ball = Bodies.circle(400, 400, 30, { restitution: 0.9 });
 const ground = Bodies.rectangle(width / 2, height * 3 / 4, width, 40, { isStatic: true });
 const left_wall = Bodies.rectangle(0, height / 2,  40, height / 2, {isStatic: true});
 const right_wall = Bodies.rectangle(width, height / 2,  40, height / 2, {isStatic: true});
+const ceiling = Bodies.rectangle(width / 2, height / 4, width, 40, {isStatic: true});
 
-Composite.add(engine.world, [ball, ground, left_wall, right_wall]);
+Composite.add(engine.world, [ball, ground, left_wall, right_wall, ceiling]);
 
-document.addEventListener("click", () => {
-  const dir = Vector.normalise({
-    x: Math.random() - 0.5,
-    y: Math.random() - 0.5
-  });
+document.addEventListener("click", function(event) {
+
+    const xPos = event.clientX;
+    const yPos = event.clientY;
+    
+    const dist_to_clicked_pixel = Math.sqrt((xPos - ball.position.x) ** 2 + 
+                                            (yPos - ball.position.y) ** 2);
+
+    if(dist_to_clicked_pixel > ball.circleRadius){
+        return;
+    }
+
+
+    // dir.x determines the horizontal movement: positive -> right, negative -> left
+    const dir = Vector.normalise({
+        x: ball.position.x - xPos,
+        y: ball.position.y - yPos
+    });
 
   Body.applyForce(ball, ball.position, {
-    x: dir.x * 0.05,
-    y: dir.y * 0.05
+    x: dir.x * (dist_to_clicked_pixel / ball.circleRadius) * 0.3,
+    y: dir.y * (dist_to_clicked_pixel / ball.circleRadius) * 0.3
   });
 });
